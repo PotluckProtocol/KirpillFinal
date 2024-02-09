7c4d5603fb985377dc78c1b05c2e8d9c5c699ff9
Potluck Protocol <89051993+PotluckProtocol@users.noreply.github.com>
2024-02-10 02:19:12 +0400

    Update hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,62 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import { minterAbi, nftAbi } from '@/abi';
+import { config, minterAddress, nftAddress } from '@/config';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { readContract, readContracts } from '@wagmi/core';
 
 type Data = {
-  name: string;
+  unrevealedTokenIds: number[];
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const unknownMintedCount = await readContract(config, {
+    abi: nftAbi,
+    address: nftAddress,
+    functionName: 'unknownMinted',
+  });
+  let checkCount;
+  const lastKnownId = 2000;
+  const unrevealedTokenIds: number[] = [];
+
+  if (Number(unknownMintedCount) > 0) {
+    checkCount = await readContract(config, {
+      abi: nftAbi,
+      address: nftAddress,
+      functionName: 'minted',
+    });
+
+    const batches = [];
+    for (let i = lastKnownId; i < Number(checkCount); i += 50) {
+      const batch = [];
+      for (let j = i; j < i + 50 && j < Number(checkCount); j++) {
+        batch.push({
+          address: minterAddress,
+          abi: minterAbi,
+          functionName: 'tokenData',
+          args: [j],
+        });
+      }
+      batches.push(batch);
+    }
+
+    for (const batch of batches) {
+      const results = await readContracts(config, { contracts: batch as any });
+      if (results) {
+        results.forEach((result, index) => {
+          console.log(result);
+          if (
+            result &&
+            result.status == 'success' &&
+            Number((result.result as any[])[1]) === 0
+          ) {
+            unrevealedTokenIds.push(batch[index].args[0]);
+          }
+        });
+      }
+    }
+  }
+
+  res.status(200).json({ unrevealedTokenIds });
 }
